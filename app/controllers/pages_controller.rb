f94cbc616416f5505b3fde84aec571c6ac9a18c9
Gordon Evans <gordon@rightbluelabs.com>
2018-04-25 00:45:49 -0400

    Updated basic sections

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def index
+    @our_work = JSON.parse(File.read('public/json/our_work.json'))
+    @clients = JSON.parse(File.read('public/json/clients.json'))
   end
 
   def casestudy_kfc
