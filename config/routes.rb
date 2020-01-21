6273e723ffb3d92289ee08ccddb9bf385426e041
Gordon Evans <gordon@rightbluelabs.com>
2020-01-21 15:48:46 -0700

    - Some UI Shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
-  get "kfc", to: "pages#kfc", as: "kfc"
+  get "kfc", to: "pages#kfc"
+  get "results", to: "pages#results"
 end
